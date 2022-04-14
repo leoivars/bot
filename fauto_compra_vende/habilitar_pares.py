0be6225f306d8c067a18513ed2282ece6cc5b9b6
Leonardo Ivars <leoivars@gmail.com>
2022-04-13 21:13:02 -0300

    adecuación al nuevo modelo-conexion

--- fauto_compra_vende/habilitar_pares.py
+++ fauto_compra_vende/habilitar_pares.py
@@ -72,8 +72,7 @@ def hay_precios_minimos_como_para_habilitar(ind:Indicadores,g:Global_State,log):
 
 def hay_parte_baja_y_volumen_impulso(ind:Indicadores,log: Logger,g:Global_State,p_xmin_impulso=26):
     ret = False
-    if filtro_rsi(ind,log,'1d',30):
-        if filtro_parte_baja_rango(ind,log,'1d',200,.236):
+    if filtro_rsi(ind,log,'1d',30) or filtro_parte_baja_rango(ind,log,'1d',200,.236):
             if filtro_parte_baja_rango(ind,log,'4h',200,.236):  
                 if filtro_parte_baja_rango(ind,log,'1h',200,.236):  
                     if filtro_xvolumen_de_impulso(ind,log,'1h',periodos=14,sentido=0,xmin_impulso=p_xmin_impulso):
